1456db75a2079ba1a29e931637e9a14ac18a8bec
Luda Ashikhmina <ludaemalah@gmail.com>
2026-02-21 11:56:30 +0000

    use clang-tidy

--- BST.c
+++ BST.c
@@ -2,105 +2,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct Node{
+typedef struct Node {
     struct Node* leftChild;
-    struct Node* rightChild; 
+    struct Node* rightChild;
     int value;
-}Node; 
+} Node;
 
-
-typedef struct BST{
+typedef struct BST {
     Node* root;
     int numOfEl;
-}BST; 
+} BST;
 
-
-BST* bstInit (void){
-    BST* tree = malloc (sizeof(BST));
-    tree -> root = NULL;
-    tree -> numOfEl = 0;
+BST* bstInit(void)
+{
+    BST* tree = malloc(sizeof(BST));
+    tree->root = NULL;
+    tree->numOfEl = 0;
     return tree;
 }
 
-int insertRecursion(Node* vertex, int n){
-    if (vertex -> value < n) {
-        if (vertex -> leftChild == NULL) {
+int insertRecursion(Node* vertex, int n)
+{
+    if (vertex->value < n) {
+        if (vertex->leftChild == NULL) {
             Node* leaf = malloc(sizeof(Node));
             if (leaf == NULL)
                 return -1;
-            leaf -> leftChild = NULL;
-            leaf -> rightChild = NULL;
-            leaf -> value = n;
-            vertex -> leftChild = leaf;
+            leaf->leftChild = NULL;
+            leaf->rightChild = NULL;
+            leaf->value = n;
+            vertex->leftChild = leaf;
             return 0;
         }
-        return insertRecursion(vertex -> leftChild, n); 
-    } 
-    else if (vertex -> value > n) {
-        if (vertex -> rightChild == NULL) {
+        return insertRecursion(vertex->leftChild, n);
+    } else if (vertex->value > n) {
+        if (vertex->rightChild == NULL) {
             Node* leaf = malloc(sizeof(Node));
             if (leaf == NULL)
                 return -1;
-            leaf -> leftChild = NULL;
-            leaf -> rightChild = NULL;
-            leaf -> value = n;
-            vertex -> rightChild = leaf;
+            leaf->leftChild = NULL;
+            leaf->rightChild = NULL;
+            leaf->value = n;
+            vertex->rightChild = leaf;
             return 0;
         }
-        return insertRecursion(vertex -> rightChild, n);
+        return insertRecursion(vertex->rightChild, n);
     }
     return 0;
 }
 
-int bstInsert(BST* tree, int value){
-    if (tree -> root == NULL) {
-       Node* vert = malloc(sizeof(Node));
-       vert -> value = value;
-       vert -> leftChild = NULL;
-       vert -> rightChild = NULL;
-       tree -> root = vert;
-       return 0;
+int bstInsert(BST* tree, int value)
+{
+    if (tree->root == NULL) {
+        Node* vert = malloc(sizeof(Node));
+        vert->value = value;
+        vert->leftChild = NULL;
+        vert->rightChild = NULL;
+        tree->root = vert;
+        return 0;
     }
-    int error = insertRecursion(tree -> root, value);
+    int error = insertRecursion(tree->root, value);
     if (error != 0)
         return -1;
-    tree -> numOfEl++;
+    tree->numOfEl++;
     return 0;
-} 
+}
 
-bool containsRecursion (Node* vertex, int n){
-    if (vertex -> value < n) {
-        if (vertex -> leftChild == NULL) {
+bool containsRecursion(Node* vertex, int n)
+{
+    if (vertex->value < n) {
+        if (vertex->leftChild == NULL) {
             return false;
         }
-        return containsRecursion(vertex -> leftChild, n); 
-    } 
-    else if (vertex -> value > n) {
-        if (vertex -> rightChild == NULL) {
+        return containsRecursion(vertex->leftChild, n);
+    } else if (vertex->value > n) {
+        if (vertex->rightChild == NULL) {
             return false;
         }
-        return containsRecursion(vertex -> rightChild, n);
+        return containsRecursion(vertex->rightChild, n);
     }
     return true;
 }
 
-bool bstContains(BST* tree, int value){
-    if (tree -> root == NULL)
+bool bstContains(BST* tree, int value)
+{
+    if (tree->root == NULL)
         return false;
-    return containsRecursion(tree -> root, value);
+    return containsRecursion(tree->root, value);
 }
 
-void bstFreeRecursion (Node* vertex){
-    if (vertex -> leftChild != NULL)
-        bstFreeRecursion( vertex -> leftChild);
-    if (vertex -> rightChild != NULL)
-        bstFreeRecursion( vertex -> rightChild); 
+void bstFreeRecursion(Node* vertex)
+{
+    if (vertex->leftChild != NULL)
+        bstFreeRecursion(vertex->leftChild);
+    if (vertex->rightChild != NULL)
+        bstFreeRecursion(vertex->rightChild);
     free(vertex);
 }
 
-void bstFree(BST* tree){
-    if (tree -> root != NULL)
+void bstFree(BST* tree)
+{
+    if (tree->root != NULL)
         bstFreeRecursion(tree->root);
     free(tree);
 }
--- BST.h
+++ BST.h
@@ -9,12 +9,11 @@ int bstInsert(BST*, int);
 bool bstContains(BST*, int);
 void bstFree(BST*);
 
-void bstInorder (BST*);
-void bstPreorder(BST*); 
+void bstInorder(BST*);
+void bstPreorder(BST*);
 void bstPostoreder(BST*);
 
-int bstHeight (BST*);
-int bstSize (BST*);
-int bstMin (BST*);
+int bstHeight(BST*);
+int bstSize(BST*);
+int bstMin(BST*);
 int bstMax(BST*);
-
