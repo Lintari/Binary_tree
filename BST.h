af94ec0752f778aa864cfa327807bff8e42430eb
Luda Ashikhmina <ludaemalah@gmail.com>
2026-02-21 11:27:07 +0000

    add struct

---
+++ BST.h
@@ -0,0 +1,17 @@
+#pragma once
+typedef struct BST BST;
+
+BST* bstInit(void);
+void bstInsert(BST*, int);
+bool bstContains(BST*, int);
+void bstFree(BST*);
+
+void bstInorder (BST*);
+void bstPreorder(BST*); 
+void bstPostoreder(BST*);
+
+int bstHeight (BST*);
+int bstSize (BST*);
+int bstMin (BST*);
+int bstMax(BST*);
+
